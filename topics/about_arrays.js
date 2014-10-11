815fa765fd1f7b3c4004b604274e332ed224a7e3
gregkorte <gryphionsroost@yahoo.com>
2014-10-11 17:14:35 -0500

    Javascript Koans: about_arrays.js.

--- topics/about_arrays.js
+++ topics/about_arrays.js
@@ -20,7 +20,7 @@ test("splice", function() {
     var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     var workingWeek = daysOfWeek.splice(0, 5);
     ok(workingWeek.equalTo(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']), 'what is the value of workingWeek?');
-    ok(daysOfWeek.equalTo(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']), 'what is the value of daysOfWeek?');
+    ok(daysOfWeek.equalTo(['Saturday', 'Sunday']), 'what is the value of daysOfWeek?');
 });
 
 test("stack methods", function() {
